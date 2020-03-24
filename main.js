63b0470536c5d20379385839dfe9c8aefc6d2991
GianmarcoMontanari <giammont@live.it>
2020-03-24 13:27:35 +0100

    inserimento variaibli + richiesta cognome utente +  .push in lista

---
+++ main.js
@@ -0,0 +1,25 @@
+
+// dichiarazioni variabili
+
+var cognomeUtente;
+var listaCognomi = ["rossi" , "bianchi" , "foti" , "vargas" , "marchi" , "zorro"]
+
+// Chiedi all’utente il cognome
+
+cognomeUtente = prompt("scrivi qua il tuo cognome");
+
+// inseriscilo in un array con altri cognomi
+
+listaCognomi.push(cognomeUtente);
+
+
+
+
+// stampa la lista ordinata alfabeticamente
+
+
+
+
+
+
+// Scrivi la posizione della lista in cui il nuovo utente si trova
