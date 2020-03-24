7569860278627b80efbbfce63cfc846dcb2d0990
GianmarcoMontanari <giammont@live.it>
2020-03-24 16:05:14 +0100

    iniezione js in html + inserimento posizione in lista

--- main.js
+++ main.js
@@ -1,29 +1,43 @@
 
-// dichiarazioni variabili
+// dichiarazioni variabili -------------------------------------------------
 
 var cognomeUtente;
-var listaCognomi = ["rossi" , "bianchi" , "foti" , "vargas" , "marchi" , "zorro"]
-
-console.log(listaCognomi);
-
-// Chiedi all’utente il cognome
-
-cognomeUtente = prompt("scrivi qua il tuo cognome");
-
-// inseriscilo in un array con altri cognomi
-
-listaCognomi.push(cognomeUtente);
-
-console.log(listaCognomi);
+var listaCognomi;
+var listaOrdinata;
+var cognome;
+var elementoLista;
+var indice;
 
 
+// dichiaro la lista array dei cognomi------------------------------------------------
+listaCognomi = ["Rossi","Bianchi","Foti","Vargas","Marchi","Zorro"]
 
 
-// stampa la lista ordinata alfabeticamente
+// Chiedi all’utente il cognome  ----------------------------------------------------
+cognomeUtente = prompt("scrivi qua il tuo cognome con la prima lettera maiuscola");
 
 
 
+// inseriscilo in un array con altri cognomi ----------------------------------------
+listaCognomi.push(cognomeUtente); //ora il cognome utente verrà inserito dentro l'array
 
 
+// ordino la lista dell'array -------------------------------------------------------
+listaOrdinata = listaCognomi.sort();
+
+
+// inizio il ciclo per creare la lista ------------------------------------------------
+var i = 0;
+while (i < listaOrdinata.length) {
+  cognome = listaOrdinata[i];
+  elementoLista = document.getElementById("lista").innerHTML;
+  i++;
+  // stampo la lista ordinata alfabeticamente--------------------------------------------
+  document.getElementById("lista").innerHTML = elementoLista + "<li>" + cognome + "</li>";
+}
 
 // Scrivi la posizione della lista in cui il nuovo utente si trova
+
+indice = (listaOrdinata.indexOf(cognomeUtente)) + 1;
+
+document.getElementById("indice").innerHTML = "Il tuo cognome si trova alla posizione numero " + indice;
